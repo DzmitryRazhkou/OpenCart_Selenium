a9415dd78f6698d86c03aa466c80461959d27302
dzmitryrazhkou <dzmitryrazhkou@gmail.com>
2022-10-27 22:20:25 -0700

    allure

--- src/test/java/com/qa/opencart/test/RegisterPageTest.java
+++ src/test/java/com/qa/opencart/test/RegisterPageTest.java
@@ -4,6 +4,10 @@ import com.github.javafaker.Faker;
 import com.qa.opencart.basetest.BaseTest;
 import com.qa.opencart.pages.RegisterPage;
 import com.qa.opencart.pages.YourStorePage;
+import io.qameta.allure.Description;
+import io.qameta.allure.Severity;
+import io.qameta.allure.SeverityLevel;
+import io.qameta.allure.Story;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
@@ -12,15 +16,21 @@ public class RegisterPageTest extends BaseTest {
     private Faker faker;
 
     @Test(priority = 1)
+    @Severity(SeverityLevel.TRIVIAL)
+    @Description("Test Case Description: Validate Register Page Title Test")
+    @Story("Story Name: To Check Register Page Title Feature")
     public void validateRegisterTitleTest() {
         YourStorePage yourStorePage = new YourStorePage(driver);
         registerPage = yourStorePage.doNavigateRegisterPage();
-        String expectedRegisterTitlePage = "Register Account";
+        String expectedRegisterTitlePage = prop.getProperty("registerTitlePage");
         String actualRegisterTitlePage = registerPage.getRegisterAccountPageTitle();
         Assert.assertEquals(expectedRegisterTitlePage, actualRegisterTitlePage);
     }
 
     @Test(priority = 2)
+    @Severity(SeverityLevel.CRITICAL)
+    @Description("Test Case Description: Do Register New Customer Test")
+    @Story("Story Name: To Check Register New Customer")
     public void doRegisterNewCustomerTest() {
         YourStorePage yourStorePage = new YourStorePage(driver);
         faker = new Faker();
@@ -37,6 +47,9 @@ public class RegisterPageTest extends BaseTest {
     }
 
     @Test(priority = 3)
+    @Severity(SeverityLevel.CRITICAL)
+    @Description("Test Case Description: Do Register New Customer Boundary Two Digits Test")
+    @Story("Story Name: To Check Register New Customer Boundary Two Digits")
     public void doRegisterNewCustomerTwoDigitsTest() {
         YourStorePage yourStorePage = new YourStorePage(driver);
         faker = new Faker();
